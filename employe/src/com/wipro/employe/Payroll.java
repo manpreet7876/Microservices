69c0f2924fe025b5866442a5dc8a2fbe71d42671
manpreet7876 <103956751+manpreet7876@users.noreply.github.com>
2025-02-22 17:19:30 +0530

    commited

---
+++ employe/src/com/wipro/employe/Payroll.java
@@ -0,0 +1,5 @@
+package com.wipro.employe;
+
+public class Payroll {
+
+}
---
+++ microservices/src/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	What is a monolithic application?
+	A) An application where all components are separate and communicate with each other
+	B) An application where all components are tightly integrated and run as a single service
+	C) A type of application that uses microservices
+	D) An application based on serverless architecture
+}
+
